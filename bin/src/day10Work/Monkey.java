8e69a52881cead3aa04dfbb1e553e0a64a608362
nizamspinebiz <nizamuddinn@spinebiz.com>
2020-06-12 00:35:18 +0530

    message

--- bin/src/day10Work/Monkey.java
+++
@@ -1,9 +0,0 @@
-package day10Work;
-
-public class Monkey extends Animal {
-	@Override
-	void move() {
-		System.out.println("Monkey Jumps..l!!!");
-		
-	}
-}
--- bin/src/day6/GoldCustomer.java
+++
@@ -1,7 +0,0 @@
-package day6;
-
-public class GoldCustomer extends Customer {
-	public void extraPoints() {
-		System.out.println("Here are your extra points 20.Jolly!!!");
-	}
-}
