8dc5674560dfe71695bd3d98118852cca1238e01
nizamspinebiz <nizamuddinn@spinebiz.com>
2020-06-21 23:04:30 +0530

    Day19Assignment

--- src/day19Assignment/Driver.java
+++ src/day19Assignment/Driver.java
@@ -1,41 +1,31 @@
 package day19Assignment;
 
-import java.util.Scanner;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Driver {
-
-	private int driverid;
-	private String drivername;
-	String driverphonenumber;
-	String busallocated;
-	ArrayList<Integer> driverList;
 	Scanner scanner;
-
-
- Driver() {
-	 scanner = new Scanner(System.in);
-	 driverList = new ArrayList<Integer>();
- }
- 
- Driver(int driverid,String drivername,String driverphonenumber){
+	ArrayList<Driver> driverlists;
+	Driver (){
 		scanner = new Scanner(System.in);
-		this.driverid = driverid;
-		this.drivername = drivername;
-		this.driverphonenumber = drivername;
-		this.busallocated = busallocated;
-		}
- 
- public int getDriverid() {
-	 
+		driverlists = new ArrayList<Driver>();
+	         }
+	
+	
+	int driverid;
+	String drivername,driverphone;
+	boolean Busallocated;
+	
+	public int getDriverid() {
 		return driverid;
 	}
 
 	public void setDriverid(int driverid) {
 		this.driverid = driverid;
 	}
-	
+
 	public String getDrivername() {
 		return drivername;
 	}
@@ -44,33 +34,183 @@ public class Driver {
 		this.drivername = drivername;
 	}
 
- void GetDriverDetailsFromUser() {
-		System.out.println("Please enter the driver id");
-		driverid = scanner.nextInt();
-		scanner.nextLine();
-		System.out.println("Please enter the driver name");
-		drivername = scanner.nextLine();
-		System.out.println("Please enter the driver Phonenumber");
-		driverphonenumber = scanner.nextLine();
-		System.out.println("Enter the allocated bus");
-		//System.out.println(maskString(phone, 0, 4, '*') );
-        busallocated = scanner.nextLine();
-     
+	public String getDriverphone() {
+		return driverphone;
+	}
+
+	public void setDriverphone(String driverphone) {
+		this.driverphone = driverphone;
+	}
+
+	public boolean isBusallocated() {
+		return Busallocated;
+	}
+	public void setBusallocated(boolean busallocated) {
+		Busallocated = busallocated;
+	}
+
+	Driver(int driverid,String drivername,String driverphone,boolean Busallocated){
+		this.driverid=driverid;
+		this.drivername=drivername;
+		this.driverphone=drivername;
+		//this.Busallocated=Busallocated;
+	}
+
+	void  phone(){
+		 do {  System.out.println("Please enter Driver Phone: ");
+         driverphone = scanner.nextLine();     
+         }while(driverphone.matches("\\d{10}")); { 
+      	  
+         driverphone = scanner.nextLine();
+         } 
+	}
+    String getDriverDetailsFromUser() {
+
+          
+
+           System.out.println("Please enter Driver ID: ");
+           driverid = scanner.nextInt();
+           scanner.nextLine();
+
+           System.out.println("Please enter Driver Name: ");
+           drivername = scanner.nextLine();
+          /* do {
+           System.out.println("Please enter Driver Phone: ");
+           driverphone = scanner.nextLine();
+           
+           }*/
+           
+           System.out.println("Please enter Driver Phone: ");
+           driverphone = scanner.nextLine();
+           if(driverphone.matches("\\d{10}"))
+           {       
+        	   
+           } else
+           {
+        	   System.out.println("!!!!Invalid phone number!!!!"); 
+        	   //System.out.println("!!!!Invalid phone number!!!!");
+            	 System.out.println("Please enter Driver Phone: ");
+            	 driverphone = scanner.nextLine();
+           }
+        	  // driverList.add(driverphone); 
+		return drivername;
+        	  
+           
+           
+     	 
+
+//         System.out.println("Please enter Driver Name: ");
+
+//         DriverName = scanner.nextLine();
+
+          
+
+    }
+    
+    @Override
+    public String toString()
+	{
+		return "\nDriverid is :"+driverid+
+				"\nDrivername is :"+drivername+
+				"\nDriverphone is :"+driverphone;
+				/*"\nBusallocated is :"+Busallocated;*/
+	}
+    
+    public void addDriverAccount() {
+		Driver driver  = new Driver();
+		driver.getDriverDetailsFromUser();
+		driverlists.add(driver);
+	}
+    Driver finddriverIndex(int id)
+	{
+		for(Driver driv :driverlists) {
+			if(driv != null)
+			{
+				if(driv.getDriverid()==driverid)
+				{
+					return driv;
+				}
+			}
+		}
+		return null;
+	}
+    
+    public void deletecustomerDetails() {
+		int driverid1=0;
+		System.out.println("Please enter the Driver id to be deleted");
+		driverid1 = scanner.nextInt();
+		Driver m1 = finddriverIndex(driverid1);
+		if(m1 == null)
+		{
+			System.out.println("No such Driver");
+			System.out.println("No Driver deleted");
+		}
+		else	
+		{
+			System.out.println("The Driver to be deleted is ");
+			System.out.println(m1);
+			driverlists.remove(m1);
+			System.out.println("Driver details deleted");
+			System.out.println("--------------------");
+		}
 			
 	}
- @Override
-	public String toString() {
-			return "Driver ID "+driverid+"\nDriver Name "+drivername+"\nCustomer phone"+driverphonenumber;
+    
+    void editDriver() {
+		int driverid1=0;
+		System.out.println("Please enter the age to be Edit");
+		driverid1 = scanner.nextInt();
+		Driver m1 = finddriverIndex(driverid1);
+		System.out.println("The details for Edition");
+		if(m1==null)
+		{
+			System.out.println("No such details");
+		}
+		else
+		{
+			System.out.println(m1);
+			System.out.println("--------------------");
+			
+			System.out.println("Please enter the name for Edit");
+			String drivername = scanner.nextLine();
+			scanner.nextLine();
+			System.out.println("Please enter the phone for Add");
+			String driverphone = scanner.nextLine();
+			
+			//System.out.println("Please enter the customer age for updation");
+			//int age = scanner.nextInt();
+	    	m1.setDrivername(drivername);
+			m1.setDriverphone(driverphone);
+			
+			System.out.println("");
+			System.out.println("");
+			System.out.println("Edited success");
+			
+			//System.out.println("The updated  details");
+		//	System.out.println(m1);
+			System.out.println("--------------------");
+			
+		}
 	}
+    
+public void adddriverAccounts() {
+		
 
-	@Override
-    public boolean equals(Object obj) {
-	Driver d2 = (Driver)obj;
-	Driver d1 = this;
-	if(d1.getDriverid()==d2.getDriverid())
-	return true;
-	else
-	return false;
+		String choice = "No";
+		do {
+			addDriverAccount();
+			System.out.println("Added....");
+			
+			System.out.println("Do u want to Add Another Account?? Key in Yes for next entry and No for Mainmenu");
+			choice = scanner.nextLine();
+			
+		} while (!choice.toLowerCase().equals("no"));
 	}
-
+public void printdriveraccount() {
+	for (Driver driver : driverlists) {
+		System.out.println(driver);
+		System.out.println("--------------------");
+	}
+}
+  
 }
--- src/day19Assignment/Transport.java
+++ src/day19Assignment/Transport.java
@@ -1,213 +1,83 @@
 package day19Assignment;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-
-public class Transport {
-
-	//Customer[] customers;
-		ArrayList<Driver> drivers;
-		ArrayList<Busmodel> busmodels;
-		ArrayList<Employee> employees;
-		Scanner scanner;
-		//int numberOfCustomers;
-		
-		Transport(){
-			scanner = new Scanner(System.in);
-			drivers = new ArrayList<Driver>();
-			busmodels = new  ArrayList<Busmodel>();
-			employees = new ArrayList<Employee>();
-		
-		}
-		public void addDriver() {
-			Driver driver = new Driver();
-			driver.GetDriverDetailsFromUser();
-			drivers.add(driver);
-			
-			}
-		
-		public void addBus() {
-			Busmodel busmodel = new Busmodel();
-			busmodel.BusDetailsFromUser();
-			busmodels.add(busmodel);
-			}
-		
-		public void addEmployee() {
-			Employee employee = new Employee();
-			employee.getemployeeDetails();
-			employees.add(employee);
-			
-			}
-				
-		public void addDrivers() {
-			String choice = "No";
-			do {
-				addDriver();
-				System.out.println("Do you want to add Driver? Enter Yes for next entry and No for quiting");
-				choice = scanner.nextLine();
-			} while (!choice.toLowerCase().equals("no"));
-			System.out.println("Driver added");
-			System.out.println("--------------------");
-		}
-		Driver findDriverIndex(int driverid)
-		{
-				for (Driver driver : drivers) {
-				if (driver !=null)
-					{
-					if(driver.getDriverid()==driverid)
-					{
-					return driver;
-					}
-				
-					}
-				
-					}
-			return null;
-		}
-		
-		public void compareDrivers() {
-			int id1=0,id2=0;
-			Driver d1,d2;
-			System.out.println("Please enter the first driver id to be compared");
-			id1 = scanner.nextInt();
-			d1 = findDriverIndex(id1);
-			if(d1==null)
-			{
-				System.out.println("No such driver please try again from start");
-				compareDrivers();
-			}
-			System.out.println("Please enter the second customer id to be compared");
-			id2 = scanner.nextInt();
-			d2 = findDriverIndex(id2);
-			if(d2==null)
-			{
-				System.out.println("No such customer please try again from start");
-				compareDrivers();
-			}
-			if(d1.equals(d2))
-				System.out.println("The Drivers are just the same");
-			else
-				System.out.println("Not same");
-			System.out.println("--------------------");
+import java.util.ArrayList;
+public class Transport extends Employee{
+Scanner scanner;
+ArrayList<Transport> trans;
+ Transport(){
+	scanner = new Scanner(System.in);
+	trans = new ArrayList<Transport>();
+}
+void ChooseActionForUser() {
+	int userChoice = 0;
+	do {
+		System.out.println("--------------------");
+		System.out.println("Press 1 Add Driver");
+		System.out.println("Press 2 to Add Bus");
+		System.out.println("Press 3 to Add Employee");
+		System.out.println("Press 4 to Edit Driver Details");
+		System.out.println("Press 5 to Edit Bus Details");
+		System.out.println("Press 6 to Delete Driver");
+		System.out.println("Press 7 to Delete Bus");
+		System.out.println("Press 8 to DeleteEmployee");
+		System.out.println("Press 9 to Assign Driver For Bus");
+		System.out.println("Press 10 to AssignBusForEmployee");
+		System.out.println("Press 0 Exit");
+		System.out.println("--------------------");
+		userChoice  = scanner.nextInt();
+		switch (userChoice ) {
+		case 1:	
+			adddriverAccounts();
+			break;
+		case 2:	
+			addbusAccounts();
+			break;
+		case 3:	
+			addEmployeeAccounts();
+			break;
+		case 4:	
+			editDriver();
+			break;
+		case 5:
+			editbus();
+			break;
+		case 6:	
+			deletecustomerDetails();
+			break;
+		case 7:	
+			deleteBusDetails();
+			break;
+		case 8:
+			deleteEmployeeDetails();
+			break;
+		case 9:	
+			getBusDetils();
+			break;
+		case 10:
+			getEmployeeDetailsfromUser();
+			break;	
+		case 0:
+			System.out.println("Exiting...../n Thankyou");
+			break;
+		default:
+			System.out.println("Invalid option.. Please press the correct number");
+			break;
 		}
 		
-		public void deleteDriver() {
-			int id1=0;
-			System.out.println("Please enter the customer id to be deleted");
-			id1 = scanner.nextInt();
-			Driver d1 = findDriverIndex(id1);
-			if(d1==null)
-			{
-				System.out.println("No such driver");
-				System.out.println("No driver deleted");
-			}
-			else	
-			{
-				System.out.println("The Driver to be deleted is ");
-				System.out.println(d1);
-				drivers.remove(d1);
-				System.out.println("Driver deleted");
-				System.out.println("--------------------");
-			}
-		}
-		public void printCustomer() {
-			int id1=0;
-			System.out.println("Please enter the driver id to be printed");
-			id1 = scanner.nextInt();
-			Driver d1 = findDriverIndex(id1);
-			System.out.println("The selected driver details");
-			if(d1==null)
-			{
-				System.out.println("No such driver");
-			}
-			else
-				System.out.println(id1);
-			System.out.println("--------------------");
-		}
-		
-		public void printAllDriver() {
-			for (Driver driver : drivers) {
-				System.out.println(driver);
-				System.out.println("--------------------");
-			}
-		}
-		public void updateDriverDetails() {
-			int id1=0;
-		
-			System.out.println("Please enter the driver id to be updated");
-			id1 = scanner.nextInt();
-			Driver d1 = findDriverIndex(id1);
-			System.out.println("The driver details for updation");
-			if(d1==null)
-			{
-				System.out.println("No such driver");
-			}
-			else
-			{
-				System.out.println(d1);
-				System.out.println("--------------------");
-				scanner.nextLine();
-				System.out.println("Please enter the driver name for updation");
-				String drivername = scanner.nextLine();
-				scanner.nextLine();
-				d1.setDrivername(drivername);
-				System.out.println("Update success");
-				System.out.println("The updated customer details");
-				System.out.println(d1);
-				System.out.println("--------------------");
-				
-			}
-		}
-		void printDriverMenu() {
-			int userChoice = 0;
-			do {
-				System.out.println("--------------------");
-				System.out.println("1. Adding a new driver");
-				System.out.println("2. Updating the driver details");
-				System.out.println("3. Deleting a driver");
-				System.out.println("4. Printing all the driver details");
-				System.out.println("5. Printing the given driver detail");
-				System.out.println("6. Compare drivers");
-				System.out.println("7. Exit");
-				System.out.println("--------------------");
-				userChoice  = scanner.nextInt();
-				switch (userChoice ) {
-				case 1:
-					
-					addDrivers();				
-					break;
-				case 2:
-					updateDriverDetails();
-					break;
-				case 3:
-					deleteDriver();
-					break;
-				case 4:
-					printAllDriver();
-					break;
-				case 5:
-					printCustomer();
-					break;
-				case 6:
-					compareDrivers();
-					break;
-				case 7:
-					System.out.println("exit.....");
-					break;
-				default:
-					System.out.println("Invalid Entry.. Try again");
-					break;
-				}
-				
-			} while (userChoice!=7);
-			
-		}
-		
-	public static void main(String[] args) {
-		Transport tt = new Transport();
-		tt.addDrivers();
-		tt.printDriverMenu();		
-
-	}
+	} while (userChoice!=11);
+	
+}
+public static void main(String[] args) {
+	Transport obj= new Transport();
+	/*mudinchu.adddriverAccounts();
+	mudinchu.addbusAccounts();	
+	mudinchu.addEmployeeAccounts();
+	mudinchu.printdriveraccount();
+	mudinchu.printbusaccount();
+	mudinchu.printEmloyeeaccount();*/
+	obj.ChooseActionForUser();
+	
+}
 
 }
